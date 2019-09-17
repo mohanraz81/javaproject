b327657abe8377e673fbc55d52ce559f16921fd4
mohanraz81 <mohanraz@gmail.com>
2019-09-17 12:53:29 +0530

    test

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  Test Test Test" );
+        System.out.println( "Hello World  Test Test Test test" );
     }
 
 	public int calculateSomething() {
