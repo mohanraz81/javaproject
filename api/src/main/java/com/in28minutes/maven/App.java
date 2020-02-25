9a91803b31d5a817ab521570475dfed0884f9f61
Ubuntu <ubuntu@ip-172-31-18-19.us-west-2.compute.internal>
2020-02-25 07:17:20 +0000

    new change

--- api/src/main/java/com/in28minutes/maven/App.java
+++ api/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  My Code" );
+        System.out.println( "Hello World Test My Code" );
     }
 
 	public int calculateSomething() {
