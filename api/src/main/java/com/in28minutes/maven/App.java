c55239aaa4d94ef77834754fedc33263f6b80e69
Ubuntu <ubuntu@ip-172-31-18-19.us-west-2.compute.internal>
2020-02-25 06:20:34 +0000

    api changes

---
+++ api/src/main/java/com/in28minutes/maven/App.java
@@ -0,0 +1,17 @@
+package com.in28minutes.maven;
+
+/**
+ * Hello World test!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World  My Code" );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+}
