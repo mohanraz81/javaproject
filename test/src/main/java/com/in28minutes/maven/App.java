60acda19344a3c5d41bb5b9b84e04d33fe1e15bc
EC2 Default User <ec2-user@ip-172-31-30-175.us-west-2.compute.internal>
2020-03-24 09:07:53 +0000

    test123

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Test My Code Test again" );
+        System.out.println( "Hello World Test My Code Test againTest" );
     }
 
 	public int calculateSomething() {
