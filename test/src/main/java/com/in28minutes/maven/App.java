c36ad8bc2a9a2cd6f14eced5e5d545036618f921
EC2 Default User <ec2-user@ip-10-0-7-238.ec2.internal>
2020-08-11 09:04:29 +0000

    code change

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Test My Code Test againTest" );
+        System.out.println( "My New Code" );
     }
 
 	public int calculateSomething() {
